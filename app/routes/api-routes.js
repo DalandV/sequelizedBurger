21e627cd97534d1a6c07bc11df61de7761d5c5b5
Daland Dixon <dalanddixon@Dalands-MBP.fios-router.home>
2019-03-02 22:27:17 -0500

    API Routes

---
+++ app/routes/api-routes.js
@@ -0,0 +1,26 @@
+const db = require("../models");
+
+module.exports = function(app) {
+  app.post("/api/burger/add", function(req, res) {
+    db.Burger.create({
+      burger_name: req.body.burger_name
+    }).then(function(dbBurger) {
+      res.redirect("/");
+    });
+  });
+
+  app.put("/api/burger/update/:id", function(req, res) {
+    db.Burger.update(
+      {
+        devoured: 1
+      },
+      {
+        where: {
+          id: req.params.id
+        }
+      }
+    ).then(function(dbBurger) {
+      res.redirect("/");
+    });
+  });
+};
