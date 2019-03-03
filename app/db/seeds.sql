1eb45bd82a5a74829b1089319ae73b214215f25b
Daland Dixon <dalanddixon@Dalands-MBP.fios-router.home>
2019-03-02 20:46:38 -0500

    Sequelized

---
+++ app/db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Cheeseburger", 0);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Double Cheeseburger", 0);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Double Bacon Cheeseburger", 0);
