9bf09189f884c83086521cd003f4ac9b7473b01d
Daland Dixon <dalanddixon@Dalands-MBP.fios-router.home>
2019-03-02 20:50:43 -0500

    Removed create table from schema

--- app/db/schema.sql
+++ app/db/schema.sql
@@ -1,9 +1,2 @@
+DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
-USE burgers_db;
-
-CREATE TABLE burgers (
-    id INT NOT NULL AUTO_INCREMENT,
-    burger_name VARCHAR(50) NOT NULL,
-    devoured BOOLEAN NOT NULL DEFAULT 0,
-    PRIMARY KEY (id)
-);
